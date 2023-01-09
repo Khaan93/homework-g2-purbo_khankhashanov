dc4accce7f3b3b9fca558ad99e832e50eac9ba9c
Khaan93 <westtoraise@gmail.com>
2023-01-09 22:00:48 +0800

    14 lesson

---
+++ 14 Homework. JS/scripts/script.js
@@ -0,0 +1,14 @@
+// Импорт переменной users из файла users.js
+import users from "./scripts/usersBase.js";
+
+let user = prompt('Введите имя пользователя', '');
+if (users[user] !== undefined) {
+    let password = prompt('Введите пароль', '');
+    if (password == users[user]) {
+        alert(`Добро пожаловать, ${user}!`);
+    } else if (password !== users[user] || password == undefined || password == null) {
+        alert("Неправильный пароль");
+    }
+} else if (users[user] == undefined) {
+    alert("Такого пользователя не существует");
+}
---
+++ 14 Homework. JS/scripts/usersBase.js
@@ -0,0 +1,7 @@
+let users = {
+    Purbo: '0',
+    Arsalan: '1',
+    Anna: '2',
+}
+
+export default users;
